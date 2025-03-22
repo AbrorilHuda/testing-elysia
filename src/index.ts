5f59189d84c2755b9d321e19c488ac554f6e672c
abrorilhuda <moh.abrorilhuda@gmail.com>
2025-03-22 08:12:49 +0700

    fist commit alysia

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import { myDocumentation } from "./lib/doc/swagger";
+class Note {
+  constructor(public data: string[] = ["hallo"]){}
+}
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+      .use(myDocumentation)
+      .decorate('note', new Note())
+      .get("/", ({path}) => path)
+      .get("/note", ({note}) => note.data)
+      .group("/auth", app => {
+        return app
+            .get("/", () => "Hi")
+            .post("/sign-in", ({ body }) => body)
+            .put("/sign-up", ({ body }) => body)
+    })
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/lib/doc/swagger.ts
@@ -0,0 +1,10 @@
+import {swagger} from "@elysiajs/swagger"
+
+export const myDocumentation =  swagger({
+    documentation: {
+      info: {
+        title: "testing documentation by dc",
+        version: "1.0.0"
+      }
+    },
+  })
